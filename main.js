c4e0f91c076eedf0de83ea4fd2390c83eae8a644
Soufiane El-Gharbaoui <sofiangharbaoui54@gmail.com>
2022-11-20 11:14:52 +0100

    increase the performance of the website due to some features

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var yellowColor = "#ffc107";
 let dateDiv = document.querySelectorAll(".date *");
-
 let date = new Date();
 dateDiv[0].innerHTML = date.getDate();
 dateDiv[1].innerHTML = date.toLocaleString("default", { month: "short" });
@@ -8,6 +7,9 @@ dateDiv[2].innerHTML = date.getFullYear();
 
 let allSections = document.querySelectorAll("body > div[id]");
 let navBar = document.querySelectorAll(".nav-bar ul li");
+let beforeSpan = document.querySelector(".before");
+let skillsSpan = document.getElementsByClassName("percentage");
+// console.log(skillsH1[0].dataset.perc);
 
 for (let j = 0; j < allSections.length; j++) {
   const section = allSections[j];
@@ -21,10 +23,15 @@ for (let j = 0; j < allSections.length; j++) {
 
 for (let i = 0; i < navBar.length; i++) {
   const element = navBar[i];
-  element.addEventListener("click", (e) => {
+  element.addEventListener("click", () => {
     if (element.classList.contains("active")) {
       return;
     } else {
+      beforeSpan.style.animationName = "cha-eff";
+      setTimeout(() => {
+        beforeSpan.style.removeProperty("animation-name");
+      }, 1500);
+
       navBar.forEach((el) => {
         el.classList.remove("active");
       });
@@ -47,3 +54,24 @@ for (let i = 0; i < navBar.length; i++) {
     }
   });
 }
+navBar[1].addEventListener("click", () => {
+  let skillsPer = document.getElementById("skills");
+  let started = false; // function hasn't performed yet;
+  window.onscroll = () => {
+    if (window.scrollY >= skillsPer.offsetTop) {
+      if (started == false) {
+        [...skillsSpan].forEach((el) => {
+          let percentage = el.dataset.perc;
+          let counter = setInterval(() => {
+            el.textContent++;
+            if (el.textContent == percentage) {
+              clearInterval(counter);
+            }
+          }, 50);
+          // }, 50 / percentage); if I want all element end in one time
+        });
+      }
+      started = true;
+    }
+  };
+});
