5148b39d10fccb06d309845d4079837ac203e4c4
Soufiane El-Gharbaoui <sofiangharbaoui54@gmail.com>
2022-11-27 21:27:23 +0100

    I increased the performence of the website. And solved some issues.

--- main.js
+++ main.js
@@ -40,13 +40,11 @@ for (let j = 0; j < allSections.length; j++) {
 for (let i = 0; i < navBar.length; i++) {
   const element = navBar[i];
   element.addEventListener("click", () => {
-    if (element.classList.contains("active")) {
-      return;
-    } else {
+    if (!element.classList.contains("active")) {
       beforeSpan.style.animationName = "cha-eff";
       setTimeout(() => {
         beforeSpan.style.removeProperty("animation-name");
-      }, 1500);
+      }, 500);
 
       navBar.forEach((el) => {
         el.classList.remove("active");
@@ -72,20 +70,24 @@ for (let i = 0; i < navBar.length; i++) {
 }
 
 navBar[1].addEventListener("click", () => {
-  let skillsPer = document.getElementById("skills");
+  let skills = document.getElementById("skills");
   let started = false; // function hasn't performed yet;
   window.onscroll = () => {
-    if (window.scrollY >= skillsPer.offsetTop) {
+    if (window.scrollY >= skills.offsetTop) {
       if (started == false) {
         [...skillsSpan].forEach((el) => {
           let percentage = el.dataset.perc;
-          let counter = setInterval(() => {
-            el.textContent++;
-            if (el.textContent == percentage) {
-              clearInterval(counter);
-            }
-          }, 50);
-          // }, 50 / percentage); if I want all element end in one time
+
+          if (el.textContent != percentage) {
+            let counter = setInterval(() => {
+              el.textContent++;
+              if (el.textContent == percentage) {
+                clearInterval(counter);
+              }
+            }, 50);
+            // }, 1000 / percentage);
+            //  if I want all element end in one time
+          }
         });
       }
       started = true;
