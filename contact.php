bc447a2a3756fb19af36a6432c63dd2e75fdd380
Soufiane El-Gharbaoui <sofiangharbaoui54@gmail.com>
2022-11-28 09:01:10 +0100

    some changes

--- contact.php
+++ contact.php
@@ -1,17 +1,29 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
-  $email_subject = $_POST['subject']
   $message = $_POST['message'];
+?>
 
-  $email_from = 'soufiane.elgharbaoui@personal-website.com';
+<?php
+	$email_from = 'yourname@yourwebsite.com';
+
+	$email_subject = "New Form submission";
 
 	$email_body = "You have received a new message from the user $name.\n".
-                    "Here is the message:\n $message".
-
-    $to = "elgharbaoui.soufiane@outlook.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-                          
-    mail($to,$email_subject,$email_body,$headers);
+                            "Here is the message:\n $message".
 ?>
+
+<?php
+
+  $to = "elgharbaoui.soufiane@outlook.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers = "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
