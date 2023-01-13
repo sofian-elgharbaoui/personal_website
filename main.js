e158f35bd5baed5d71c72bd4a744e178cd340cd0
sofian-elgharbaoui <sofiangharbaoui54@gmail.com>
2023-01-13 12:46:59 +0100

    improce the UX and add the modal on the click event in portfolio

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import allProjectsInfo from "./projects.json" assert { type: "json" };
+
 let dateDiv = document.querySelectorAll(".date *");
 let date = new Date();
 dateDiv[0].innerHTML = date.getDate();
@@ -100,4 +102,76 @@ function increaseNumsOnScroll(section, liOfNums) {
       started = true;
     }
   };
+}
+
+// // // place all projects at the portfolio
+let projects = document.querySelector(".projects");
+
+for (let i = 0; i < allProjectsInfo.length; i++) {
+  let projectDiv = document.createElement("div");
+  projectDiv.className = "project";
+  projectDiv.id = allProjectsInfo[i].id;
+  projectDiv.innerHTML = `
+    <img src="images/${allProjectsInfo[i].imgs[0]}" alt="${allProjectsInfo[i]["project type"]}" />
+    <h2>${allProjectsInfo[i]["project name"]}</h2>
+  `;
+
+  projects.appendChild(projectDiv);
+}
+//
+
+let allProjects = document.querySelectorAll(".project");
+
+let modalPortfolio = document.querySelector(".modal_portfolio");
+let modalCloser = document.querySelector(".close_modal");
+let projectName = document.querySelector("#project_name");
+let projectType = document.querySelector("#project_type");
+let projectClient = document.querySelector("#project_client");
+let projectLanguages = document.querySelector("#project_languages");
+let projectPreview = document.querySelector("#project_preview");
+let projectImgs = document.querySelector(".imgs_container");
+let projectImgsPag = document.querySelector(".imgs_pagination");
+
+for (let i = 0; i < allProjects.length; i++) {
+  const project = allProjects[i];
+
+  project.addEventListener("click", () => {
+    modalPortfolio.classList.add("active");
+
+    let myInfo = allProjectsInfo[i];
+
+    projectName.innerText = myInfo["project name"];
+    projectType.innerText = myInfo["project type"];
+    projectClient.innerText = myInfo.client;
+    projectLanguages.innerText = myInfo.languages;
+
+    projectPreview.innerText = myInfo.hostname;
+    projectPreview.setAttribute("href", myInfo.url);
+
+    projectImgs.innerHTML = "";
+    projectImgsPag.innerHTML = "";
+
+    for (let i = 0; i < myInfo.imgs.length; i++) {
+      let img = document.createElement("img");
+      img.setAttribute("src", `images/${myInfo.imgs[i]}`);
+      projectImgs.append(img);
+
+      let liItem = document.createElement("li");
+      projectImgsPag.append(liItem);
+    }
+
+    let projectImgsPagItms = [...projectImgsPag.children];
+    projectImgsPagItms.forEach((itm, i) => {
+      itm.addEventListener("click", () => {
+        projectImgsPagItms.forEach((li) => li.classList.remove("active"));
+        itm.classList.add("active");
+        projectImgs.scrollTo({
+          left: 608 * i,
+          behavior: "smooth",
+        });
+      });
+    });
+
+    modalCloser.onclick = () => modalPortfolio.classList.remove("active");
+  });
 }
